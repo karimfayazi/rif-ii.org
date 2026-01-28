f4c5607a3fae4402a6a88820ff0223ba8ea33e5a
Karim Khan Fayazi <karim.fayazi@sjdap.local>
2026-01-28 19:00:20 +0500

    Fix Chart.js plugins TypeScript error
    Cast plugins prop as any to resolve TypeScript compatibility issues with ChartDataLabels plugin across all chart types (Pie, Bar, Line).

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -137,7 +137,7 @@ function DynamicChartRenderer({ chartType, data, options, height = '280px' }: Dy
 		
 		return (
 			<div style={{ height }}>
-				<Pie data={data as any} options={pieOptions} plugins={[ChartDataLabels]} />
+				<Pie data={data as any} options={pieOptions} plugins={[ChartDataLabels] as any} />
 			</div>
 		);
 	} else if (chartType === 'horizontal-bar') {
@@ -185,7 +185,7 @@ function DynamicChartRenderer({ chartType, data, options, height = '280px' }: Dy
 		
 		return (
 			<div style={{ height }}>
-				<Bar data={data as any} options={horizontalOptions} plugins={[ChartDataLabels]} />
+				<Bar data={data as any} options={horizontalOptions} plugins={[ChartDataLabels] as any} />
 			</div>
 		);
 	} else if (chartType === 'area') {
@@ -201,7 +201,7 @@ function DynamicChartRenderer({ chartType, data, options, height = '280px' }: Dy
 		
 		return (
 			<div style={{ height }}>
-				<Line data={areaData as any} options={options as ChartOptions<'line'>} plugins={[ChartDataLabels]} />
+				<Line data={areaData as any} options={options as ChartOptions<'line'>} plugins={[ChartDataLabels] as any} />
 			</div>
 		);
 	} else if (chartType === 'line') {
@@ -215,14 +215,14 @@ function DynamicChartRenderer({ chartType, data, options, height = '280px' }: Dy
 		
 		return (
 			<div style={{ height }}>
-				<Line data={lineData as any} options={options as ChartOptions<'line'>} plugins={[ChartDataLabels]} />
+				<Line data={lineData as any} options={options as ChartOptions<'line'>} plugins={[ChartDataLabels] as any} />
 			</div>
 		);
 	} else {
 		// Default: vertical bar chart
 		return (
 			<div style={{ height }}>
-				<Bar data={data as any} options={options as ChartOptions<'bar'>} plugins={[ChartDataLabels]} />
+				<Bar data={data as any} options={options as ChartOptions<'bar'>} plugins={[ChartDataLabels] as any} />
 			</div>
 		);
 	}
